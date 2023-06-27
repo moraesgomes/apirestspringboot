b4c9f16d4bb03f4b44918ee09c69992651562573
haroldo gomes <moraesgomes@gmail.com>
2023-06-26 21:38:34 -0300

    ajuste DTOUser - incluisão

--- src/main/java/curso/apirest/model/UsuarioDTO.java
+++ src/main/java/curso/apirest/model/UsuarioDTO.java
@@ -1,43 +1,73 @@
 package curso.apirest.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-public class UsuarioDTO implements Serializable{
+public class UsuarioDTO implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
-	
+
 	private Long id;
-	
+
 	private String login;
-	
+
 	private String nome;
-	
+
 	private String senha;
-	
+
 	private String cpf;
-	
+
+	private Date dataNascimento;
+
+	private BigDecimal salario;
+
+	private Profissao profissao;
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public BigDecimal getSalario() {
+		return salario;
+	}
+
+	public void setSalario(BigDecimal salario) {
+		this.salario = salario;
+	}
+
+	public Profissao getProfissao() {
+		return profissao;
+	}
+
+	public void setProfissao(Profissao profissao) {
+		this.profissao = profissao;
+	}
+
 	private List<Telefone> telefones = new ArrayList<Telefone>();
-	
-	public UsuarioDTO(Usuario usuario , List<Telefone> fones) {
-		
+
+	public UsuarioDTO(Usuario usuario, List<Telefone> fones) {
+
 		this.id = usuario.getId();
 		this.login = usuario.getLogin();
 		this.nome = usuario.getNome();
 		this.senha = usuario.getSenha();
 		this.cpf = usuario.getCpf();
-		
+		this.dataNascimento = usuario.getDataNascimento();
+		this.profissao = usuario.getProfissao();
+		this.salario = usuario.getSalario();
+
 		for (Telefone telefone : fones) {
-			 this.telefones.add(telefone);
-        }
-		
+			this.telefones.add(telefone);
+		}
+
 	}
-	
-	
-	
-	
 
 	public Long getId() {
 		return id;
@@ -78,18 +108,13 @@ public class UsuarioDTO implements Serializable{
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-	
+
 	public void setTelefones(List<Telefone> telefones) {
 		this.telefones = telefones;
 	}
-	
+
 	public List<Telefone> getTelefones() {
 		return telefones;
 	}
-
-	
-
-
-	
 
 }
